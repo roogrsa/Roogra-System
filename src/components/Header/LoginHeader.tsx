a4306fed52135f842b7fe3fca796135af8318d7b
marim27 <117209557+marim27@users.noreply.github.com>
2024-10-09 22:13:36 +0300

    Update LoginHeader.tsx

--- src/components/Header/LoginHeader.tsx
+++ src/components/Header/LoginHeader.tsx
@@ -15,7 +15,7 @@ const LoginHeader = () => {
   }, []);
   return (
     <>
-    <header dir="ltr" className={`sticky top-0 z-999 flex w-full  drop-shadow-1 ${colorMode=== 'light'?'bg-white':'bg-darkColor'} 
+    <header dir="ltr" className={`sticky top-0 z-999 flex w-full bg-white  drop-shadow-1 dark:bg-[#1E1E26] 
     dark:bg-darkColor dark:drop-shadow-none`}>
       <div className="flex flex-grow items-center justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11">
 
