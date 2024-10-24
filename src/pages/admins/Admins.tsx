ed1452492dc63d6b22a5168019278ad8e6577650
EsraaFathi <138526584+EsraaFathi@users.noreply.github.com>
2024-10-24 11:59:50 +0300

    last

--- src/pages/admins/Admins.tsx
+++ src/pages/admins/Admins.tsx
@@ -1,19 +1,25 @@
-import Breadcrumb from "../../components/Breadcrumbs/Breadcrumb";
-import { useTranslation } from "react-i18next";
-import { CgAddR } from "react-icons/cg";
-import { Link } from "react-router-dom";
+import Breadcrumb from '../../components/Breadcrumbs/Breadcrumb';
+import { useTranslation } from 'react-i18next';
+import { CgAddR } from 'react-icons/cg';
+import { Link } from 'react-router-dom';
+import AdminsList from '../../components/admins/AdminList';
 
 export default function Admins() {
-    const { t } = useTranslation();
-    const breadcrumbLinks = [{ label: t('admins.label'), path: '/admins' }]
-    return (
-        <>
-        <div className="flex justify-between">
-        <Breadcrumb pageName={t('admins.title')} breadcrumbLinks={breadcrumbLinks}/>
+  const { t } = useTranslation();
+  const breadcrumbLinks = [{ label: t('admins.label'), path: '/admins' }];
+
+  return (
+    <>
+      <div className="flex justify-between">
+        <Breadcrumb
+          pageName={t('admins.title')}
+          breadcrumbLinks={breadcrumbLinks}
+        />
         <Link to={`/admins/add-admin`}>
-            <CgAddR className="text-3xl text-Input-TextGreen" role="button"/>
+          <CgAddR className="text-3xl text-Input-TextGreen" role="button" />
         </Link>
-        </div>
-        </>
-    )
+      </div>
+      <AdminsList />
+    </>
+  );
 }
