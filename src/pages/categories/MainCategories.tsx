9f2d1ee1b12067db4dc0772f302eec110f91b04c
EsraaFathi <138526584+EsraaFathi@users.noreply.github.com>
2024-11-12 10:48:37 +0200

    edit

--- src/pages/categories/MainCategories.tsx
+++ src/pages/categories/MainCategories.tsx
@@ -1,16 +1,21 @@
-import React, { useEffect, useState } from "react";
-import { DragDropContext, Droppable, Draggable, DropResult } from "@hello-pangea/dnd";
-import Breadcrumb from "../../components/Breadcrumbs/Breadcrumb";
-import { Link } from "react-router-dom";
-import { CgAddR } from "react-icons/cg";
-import { useTranslation } from "react-i18next";
-import { RiDeleteBin6Line } from "react-icons/ri";
-import axiosInstance from "../../axiosConfig/instanc";
-import { FiEdit3 } from "react-icons/fi";
-import Pagination from "../../components/pagination/Pagination";
-import DeletePopup from "../../components/popups/DeletePopup";
-import EditAddImgPopup from "../../components/popups/EditAddImgPopup";
-import { toast } from "react-toastify";
+import React, { useEffect, useState } from 'react';
+import {
+  DragDropContext,
+  Droppable,
+  Draggable,
+  DropResult,
+} from '@hello-pangea/dnd';
+import Breadcrumb from '../../components/Breadcrumbs/Breadcrumb';
+import { Link } from 'react-router-dom';
+import { CgAddR } from 'react-icons/cg';
+import { useTranslation } from 'react-i18next';
+import { RiDeleteBin6Line } from 'react-icons/ri';
+import axiosInstance from '../../axiosConfig/instanc';
+import { FiEdit3 } from 'react-icons/fi';
+import Pagination from '../../components/pagination/Pagination';
+import DeletePopup from '../../components/popups/DeletePopup';
+import EditAddImgPopup from '../../components/popups/EditAddImgPopup';
+import { toast } from 'react-toastify';
 
 interface Category {
   parent_id: number;
@@ -19,7 +24,7 @@ interface Category {
   parent_sort_order: number;
 }
 
-type ModalType = "DELETE" | "ADD" | "EDIT" | null;
+type ModalType = 'DELETE' | 'ADD' | 'EDIT' | null;
 
 const MainCategories: React.FC = () => {
   const { t } = useTranslation();
@@ -27,7 +32,9 @@ const MainCategories: React.FC = () => {
   const [currentPage, setCurrentPage] = useState<number>(0);
   const [categoriesCount, setCategoriesCount] = useState<number>(0);
   const [modalType, setModalType] = useState<ModalType>(null);
-  const [selectedCategory, setSelectedCategory] = useState<Category | null>(null);
+  const [selectedCategory, setSelectedCategory] = useState<Category | null>(
+    null,
+  );
 
   useEffect(() => {
     const fetchCategoriesCount = async () => {
@@ -35,7 +42,7 @@ const MainCategories: React.FC = () => {
         const response = await axiosInstance.get(`/api/categories/count`);
         setCategoriesCount(Math.ceil(response.data.data.count / 8));
       } catch (err) {
-        toast.error(t("categoriesPage.errorFetchingCount"));
+        toast.error(t('categoriesPage.errorFetchingCount'));
       }
     };
     fetchCategoriesCount();
@@ -46,12 +53,12 @@ const MainCategories: React.FC = () => {
       await axiosInstance.patch(
         `/api/categories`,
         { categories: [{ id, order }] },
-        { headers: { "content-type": "Application/json" } }
+        { headers: { 'content-type': 'Application/json' } },
       );
       displayCategories();
-      toast.success(t("categoriesPage.categoriesToast"));
+      toast.success(t('categoriesPage.categoriesToast'));
     } catch (error) {
-      toast.error(t("categoriesPage.errorUpdatingOrder"));
+      toast.error(t('categoriesPage.errorUpdatingOrder'));
       console.error(error);
     }
   };
@@ -70,10 +77,17 @@ const MainCategories: React.FC = () => {
 
   const displayCategories = async () => {
     try {
-      const res = await axiosInstance.get(`/api/categories?page=${currentPage}&limit=8`);
-      setCategories(res.data.data.sort((a: Category, b: Category) => a.parent_sort_order - b.parent_sort_order));
+      const res = await axiosInstance.get(
+        `/api/categories?page=${currentPage}&limit=8`,
+      );
+      setCategories(
+        res.data.data.sort(
+          (a: Category, b: Category) =>
+            a.parent_sort_order - b.parent_sort_order,
+        ),
+      );
     } catch (error) {
-      toast.error(t("categoriesPage.errorFetchingCategories"));
+      toast.error(t('categoriesPage.errorFetchingCategories'));
       console.error(error);
     }
   };
@@ -87,53 +101,98 @@ const MainCategories: React.FC = () => {
     setModalType(type);
   };
 
-  const breadcrumbLinks = [{ label: t("categoriesPage.title"), path: "/categories/main" }];
+  const breadcrumbLinks = [
+    { label: t('categoriesPage.title'), path: '/categories/main' },
+  ];
   return (
     <div className="relative overflow-x-auto">
       <div className="flex justify-between">
-        <Breadcrumb pageName={t("categoriesPage.catMain.label")} breadcrumbLinks={breadcrumbLinks} />
+        <Breadcrumb
+          pageName={t('categoriesPage.catMain.label')}
+          breadcrumbLinks={breadcrumbLinks}
+        />
         <Link to={``}>
-          <CgAddR className="text-3xl text-Input-TextGreen" role="button" onClick={() => openModal("ADD")} />
+          <CgAddR
+            className="text-3xl text-Input-TextGreen"
+            role="button"
+            onClick={() => openModal('ADD')}
+          />
         </Link>
       </div>
       <DragDropContext onDragEnd={handleOnDragEnd}>
         <Droppable droppableId="categories">
           {(provided) => (
-            <div className="bg-secondaryBG-light dark:bg-secondaryBG-dark p-6 rounded-sm">
-              <table {...provided.droppableProps} ref={provided.innerRef} className="w-full text-left rtl:text-right text-[20px]">
+            <div className="bg-secondaryBG-light dark:bg-secondaryBG-dark">
+              <table
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                className="w-full text-left rtl:text-right text-[20px]"
+              >
                 <thead className="bg-[#EDEDED] dark:bg-[#3E3E46]">
-                  <tr className="px-2 py-2 text-[18px] font-[400]">
-                    <th className="px-2 py-3 text-[18px] font-[400] rounded-s-lg">{t("categoriesPage.order")}</th>
-                    <th className="px-6 py-3 text-[18px] font-[400]">{t("categoriesPage.catMain.catName")}</th>
-                    <th className="px-6 py-3 text-[18px] font-[400]">{t("categoriesPage.catMain.img")}</th>
-                    <th className="py-3 text-[18px] font-[400]">{t("categoriesPage.edit")}</th>
-                    <th className="py-3 rounded-e-lg">
+                  <tr className="px-2 py-8 text-[20px] font-[400]">
+                    <th className="px-2 py-6 text-[20px] font-[400] ">
+                      {t('categoriesPage.order')}
+                    </th>
+                    <th className="px-6 py-6 text-[20px] font-[400]">
+                      {t('categoriesPage.catMain.catName')}
+                    </th>
+                    <th className="px-6 py-6 text-[20px] font-[400]">
+                      {t('categoriesPage.catMain.img')}
+                    </th>
+                    <th className="py-6 text-[20px] font-[400]">
+                      {t('categoriesPage.edit')}
+                    </th>
+                    <th className="py-3 ">
                       <RiDeleteBin6Line className="text-xl text-Input-TextRed" />
                     </th>
                   </tr>
                 </thead>
                 <tbody>
                   {categories.map((cat, index) => (
-                    <Draggable key={`cat-${index}`} draggableId={`cat-${cat.parent_id}-${index}`} index={index}>
+                    <Draggable
+                      key={`cat-${index}`}
+                      draggableId={`cat-${cat.parent_id}-${index}`}
+                      index={index}
+                    >
                       {(provided, snapshot) => (
                         <tr
                           ref={provided.innerRef}
                           {...provided.draggableProps}
                           {...provided.dragHandleProps}
-                          className={`dark:border-secondaryBG-light ${index % 2 !== 0
-                            ? "dark:bg-MainTableBG-OddDark bg-MainTableBG-OddLight"
-                            : "dark:bg-MainTableBG-EvenDark bg-MainTableBG-EvenLight"} ${snapshot.isDragging ? "bg-header-inputBorder" : ""}`}
+                          className={`dark:border-secondaryBG-light ${
+                            index % 2 !== 0
+                              ? 'dark:bg-MainTableBG-OddDark bg-MainTableBG-OddLight'
+                              : 'dark:bg-MainTableBG-EvenDark bg-MainTableBG-EvenLight'
+                          } ${
+                            snapshot.isDragging ? 'bg-header-inputBorder' : ''
+                          }`}
                         >
-                          <td className="px-2 py-4 font-[400] text-[17px]">#{index + 1} {cat.parent_sort_order}</td>
+                          <td className="px-2 py-4 font-[400] text-[17px]">
+                            #{index + 1} {cat.parent_sort_order}
+                          </td>
                           <td className="px-6 py-4 font-[400] text-[17px] text-gray-900 whitespace-nowrap dark:text-white">
                             {cat.category_name}
                           </td>
-                          <td className="px-6 py-4"><img src={cat.parent_image} alt="" width={40} /></td>
-                          <td className="py-4" onClick={() => openModal("EDIT", cat)}>
-                            <FiEdit3 className="text-xl text-Input-TextGreen" role="button" />
+                          <td className="px-6 py-4">
+                            <img src={cat.parent_image} alt="" width={40} />
                           </td>
-                          <td className="py-4" onClick={() => openModal("DELETE", cat)}>
-                            <RiDeleteBin6Line className="text-xl text-Input-TextRed" role="button" />
+                          <td
+                            className="py-4"
+                            onClick={() => openModal('EDIT', cat)}
+                          >
+                            <FiEdit3
+                              className="text-xl text-Input-TextGreen"
+                              role="button"
+                            />
+                          </td>
+                          <td
+                            className="py-4"
+                            onClick={() => openModal('DELETE', cat)}
+                          >
+                            <RiDeleteBin6Line
+                              className="text-xl text-Input-TextRed"
+                              role="button"
+                            />
                           </td>
                         </tr>
                       )}
@@ -147,22 +206,22 @@ const MainCategories: React.FC = () => {
         </Droppable>
       </DragDropContext>
 
-      {modalType === "DELETE" && selectedCategory && (
+      {modalType === 'DELETE' && selectedCategory && (
         <DeletePopup
           deleteName={selectedCategory.category_name}
           deleteId={selectedCategory.parent_id}
           url={`categories`}
-          isModalOpen={modalType === "DELETE"}
+          isModalOpen={modalType === 'DELETE'}
           setIsModalOpen={() => setModalType(null)}
           display={displayCategories}
         />
       )}
-      {(modalType === "EDIT" || modalType === "ADD") && (
+      {(modalType === 'EDIT' || modalType === 'ADD') && (
         <EditAddImgPopup
           name={selectedCategory?.category_name}
           id={selectedCategory?.parent_id}
           url={`categories`}
-          isModalOpen={modalType === "EDIT" || modalType === "ADD"}
+          isModalOpen={modalType === 'EDIT' || modalType === 'ADD'}
           setIsModalOpen={() => setModalType(null)}
           display={displayCategories}
           imageUrl={selectedCategory?.parent_image}
