e348edaf3407b331c33111da65126d8cecee2ba4
marim27 <117209557+marim27@users.noreply.github.com>
2024-11-26 16:15:43 +0200

    Update BanImage.tsx

--- src/components/reports/BanImage.tsx
+++ src/components/reports/BanImage.tsx
@@ -1,28 +1,9 @@
-import { useEffect } from "react";
-import axiosInstance from "../../axiosConfig/instanc";
 
 interface BanImageProps {
-    id: number;
     isBan: boolean;
-    setIsBan: (isBan: boolean) => void;
 }
 
-export default function BanImage({ id, setIsBan, isBan }: BanImageProps) {
-
-    // const fetchUser = async () => {
-    //     try {
-    //         const response = await axiosInstance.get(`/api/users/${id}`);
-    //         const isBanned = response.data.data.isBanned;
-    //         setIsBan(isBanned == 1);
-    //     } catch (err) {
-    //         console.error("Error fetching user data:", err);
-    //     }
-    // };
-
-    // useEffect(() => {
-    //     fetchUser();
-    // }, [id]);
-
+export default function BanImage({ isBan }: BanImageProps) {
     return (
         <div
             className={`${isBan ? "bg-[#FA9623]" : "bg-[#767876]"} p-1 rounded-md flex items-center justify-center`}>
