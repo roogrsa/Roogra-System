d51fb82bb8e6d22e5f8be81d4df83d61e66fe105
marim27 <marimmohmed@outlook.com>
2024-12-18 23:49:14 +0200

    sidebar

--- src/components/Sidebar/DropLink.tsx
+++ src/components/Sidebar/DropLink.tsx
@@ -4,15 +4,17 @@ import { NavLink } from "react-router-dom";
 interface DropLinkProps {
     to: string;
     text: string;
+    closeSideBar:() => void;
 }
 
-export default function DropLink({ to, text }: DropLinkProps) {
+export default function DropLink({ to, text, closeSideBar }: DropLinkProps) {
     const { t } = useTranslation();
     return (
         <li className='text-center'>
             <NavLink
                 to={to}
                 className={`text-white duration-300 ease-in-out`}
+                onClick={closeSideBar}
             >
                 {t(text)}
             </NavLink>
--- src/components/Sidebar/SidebarLink.tsx
+++ src/components/Sidebar/SidebarLink.tsx
@@ -5,18 +5,21 @@ interface SidebarLinkProps {
   isOpen: boolean;
   text: string;
   icon: any;
+  closeSideBar?:() => void;
 }
 export default function SidebarLink({
   to,
   isOpen,
   text,
   icon,
+  closeSideBar
 }: SidebarLinkProps) {
   const { t } = useTranslation();
   const { pathname } = useLocation();
   return (
     <li>
       <NavLink
+      onClick={closeSideBar}
         to={to}
         className={`group relative flex items-center gap-2.5  py-2  text-[20px] font-[400] text-white duration-300 ease-in-out hover:bg-[#2E2D3D] rounded-[15px] ${
           pathname === to && 'bg-graydark dark:bg-meta-4'
