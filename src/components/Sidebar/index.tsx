76e9101bb2cf66008d9f3773902d994a48afb88d
marim27 <marimmohmed@outlook.com>
2024-12-25 00:04:07 +0200

    Update index.tsx

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -546,11 +546,11 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen, setIsOpen, isOpen }: SidebarProp
               onClick={logout}
               className={`bg-[#E02828] ${
                 isOpen
-                  ? 'brightness-150 w-full flex justify-center'
+                  ? 'brightness-150 w-full flex'
                   : 'hover:brightness-150 w-auto'
               } text-white text-[20px] p-2 rounded-xl transition-all duration-300`}
             >
-              <PiPowerFill className="text-2xl me-2" />
+              <PiPowerFill className="text-2xl me-3" />
               {isOpen && <div> {t('sidebar.sign-out')} </div>}
             </div>
           </nav>
